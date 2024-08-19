e6e30737535bdc6fe064ea648c948cbef11237b2
anwershamim <116711962+anwershamim@users.noreply.github.com>
2024-08-19 15:19:17 +0530

    Add files via upload

---
+++ checkingLeapYear.java
@@ -0,0 +1,15 @@
+public class checkingLeapYear {
+    public static void main(String [] args){
+    }
+    public static void isleapYear(int year){
+        switch (year){
+            case 1:
+                System.out.println( year % 4 + "true");
+                break;
+
+        }
+    }
+
+
+
+}
